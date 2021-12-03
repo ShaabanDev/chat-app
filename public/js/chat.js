3c6cac08ff6e7f7237795e9eab6068042bcefd1e
Mohamed <qotozer@gmail.com>
2021-12-03 17:37:42 +0200

    allow client to send messages to all other clients

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,14 @@
-io()
+const socket = io();
+socket.on('welcomeMessage',(message)=>{
+    console.log(message);
+})
+socket.on("receiveMessage", (message) => {
+  console.log(message);
+});
+
+
+document.querySelector('#message-form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = e.target.elements.message.value;
+    socket.emit("messageSent", message);
+})
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 // import express
-const express = require('express');
+const express = require("express");
 // import path module to help defining the public folder path
-const path = require('path');
-// import http module 
-const http = require('http');
-const socketIO = require('socket.io');
+const path = require("path");
+// import http module
+const http = require("http");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+const pubPath = path.join(__dirname, "../public");
+app.use(express.static(pubPath));
 
-const pubPath = path.join(__dirname,'../public')
-app.use(express.static(pubPath))
-
-io.on('connection',()=>{
-    console.log('new websocket connection');
-})
-
-app.get('/',(req,res)=>{
-    res.render();
+io.on("connection", (socket) => {
+    socket.emit('welcomeMessage','Welcome to our chat')
+  socket.on("messageSent", (message) => {
+    io.emit("receiveMessage", message);
+  });
 });
 
+app.get("/", (req, res) => {
+  res.render();
+});
 
-server.listen(process.env.PORT,()=>{
-    console.log('server is on port '+process.env.PORT);
-})
-
+server.listen(process.env.PORT, () => {
+  console.log("server is on port " + process.env.PORT);
+});
