be02cd9a775ec1ed23e69da0899252e5dbcdd7ed
Mohamed <qotozer@gmail.com>
2021-12-03 16:10:05 +0200

    create server using http module and instal socket.io module

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@
 const express = require('express');
 // import path module to help defining the public folder path
 const path = require('path');
+const http = require('http');
+
 const app = express();
+const server = http.createServer(app);
+
 const pubPath = path.join(__dirname,'../public')
 app.use(express.static(pubPath))
 
 app.get('/',(req,res)=>{
-    res.render()
+    res.render();
 });
 
 
