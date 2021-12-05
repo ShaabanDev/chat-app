28a1d4d79a6cfe845f8ea591335ec0e90cff8832
Mohamed <qotozer@gmail.com>
2021-12-05 22:38:30 +0200

    create adduser function

---
+++ src/utils/users.js
@@ -0,0 +1,26 @@
+const users = [];
+
+const addUser = ({ id, username, room }) => {
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  if (!username || !room) {
+    return {
+      error: "Please provide the username and room name.",
+    };
+  }
+
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+
+  if (existingUser) {
+    return {
+      error: "Username is in use!",
+    };
+  }
+
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
